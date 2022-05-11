035e09c4238757d4b1e96b2756bc661b5a6ebcca
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-11 17:11:50 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Card from "./components/Card";
-
+import Joke from "./components/Joke";
+import JokeData from "./JokesData";
+// console.log(JokesData);
+// Jokes props
 export default function App() {
+    // const jokeElement = JokeData.map((joke) => <Joke joke />);
+    const jokeElement = JokeData.map((joke) => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />;
+    });
+    return <div>{jokeElement}</div>;
+}
+
+/* 
+// colors Map
+export default function App() {
+    const colors = [
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Blue",
+        "Indigo",
+        "Violet",
+    ];
     return (
-        <div className="airbnb">
-            <div className="cardContainer">
-                <Navbar />
-                <Hero />
-                <div className="cardsContainer">
-                    <Card
-                        img="profile.jpg"
-                        alt="profile"
-                        rating={5.0}
-                        reviewCount={6}
-                        country="USA"
-                        title="Life Lessons with Katie Zaferes"
-                        price={136}
-                    />
-                    <Card
-                        img="profile.jpg"
-                        alt="profile"
-                        rating={5.0}
-                        reviewCount={6}
-                        country="USA"
-                        title="Life Lessons with Katie Zaferes"
-                        price={136}
-                    />
-                </div>
-            </div>
+        <div>
+            {colors.map((color) => (
+                <h3>{color}</h3>
+            ))}
         </div>
     );
-}
+} */
